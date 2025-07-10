4adb70164bf91cb08d583a192c465ae480ee2316
Christian <christianreynoso96@gmail.com>
2025-07-11 06:09:58 +0800

    Add useClickOutside

---
+++ src/composables/useClickOutside.ts
@@ -0,0 +1,19 @@
+import { onMounted, onBeforeUnmount } from 'vue';
+
+export function useClickOutside(targetRef: Ref<HTMLElement | null>, callback: () => void) {
+  const handleClick = (event: MouseEvent) => {
+    const el = targetRef.value;
+    if (!el) return;
+    if (!(el === event.target || el.contains(event.target as Node))) {
+      callback();
+    }
+  };
+
+  onMounted(() => {
+    document.addEventListener('click', handleClick);
+  });
+
+  onBeforeUnmount(() => {
+    document.removeEventListener('click', handleClick);
+  });
+}
