5a12197df3e867fd7af989bdb0935814b57e694e
Christian <christianreynoso96@gmail.com>
2025-07-19 12:18:43 +0800

    Allow empty strings some types

--- src/types/options.ts
+++ src/types/options.ts
@@ -5,7 +5,7 @@ import {
   MARITAL_STATUSES
 } from './constants';
 
-export type Salutation = typeof SALUTATIONS[number];
-export type Country = typeof COUNTRIES[number];
-export type Gender = typeof GENDERS[number];
-export type MaritalStatus = typeof MARITAL_STATUSES[number];
+export type Salutation = typeof SALUTATIONS[number] | '';
+export type Country = typeof COUNTRIES[number] | '';
+export type Gender = typeof GENDERS[number] | '';
+export type MaritalStatus = typeof MARITAL_STATUSES[number] | '';
